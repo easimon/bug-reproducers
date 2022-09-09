b42e65ff42d77af7a9ad47e5acfd8c88ce0a08c5
Markus Dobel <github@spam.dobel.click>
2022-09-09 12:54:52 +0200

    add runtime test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello");
-  }
+
+    private static String get(String url) throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(url))
+            .GET()
+            .build();
+
+        HttpResponse<String> response = HttpClient.newBuilder()
+            .build()
+            .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("HTTP:  " + get("http://worldtimeapi.org/api/timezone/Europe/Berlin"));
+        System.out.println("HTTPs: " + get("https://worldtimeapi.org/api/timezone/Europe/Berlin"));
+    }
 }
